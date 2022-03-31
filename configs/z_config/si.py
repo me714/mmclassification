55b47ce92218b9cb825c18084919650b33d709e5
Sunzhiqiang <szqqishi@163.com>
2022-03-31 10:38:13 +0800

    add my_config

---
+++ configs/z_config/si.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+# @Project  :mmclassification
+# @File     :si
+# @Date     :2022/3/31 10:37
+# @Author   :Sun
+# @Email    :szqqishi@163.com
+# @Software :PyCharm
+-------------------------------------------------
+"""
